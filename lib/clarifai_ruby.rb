8c93aac7401d6d5eca6e111ac9397670d193ccbb
Charlyn G <chardane@gmail.com>
2016-02-01 07:58:30 -0800

    add ability to configure gem
    This allows us to configure values for the gem to use later.
    Configuration values added were:
    
    ```
    - client_id
    - client_secret
    - base_url
    - version_path
    ```
    
    The `api_path` is calculated based on the `base_path` and `version_path`

--- lib/clarifai_ruby.rb
+++ lib/clarifai_ruby.rb
@@ -1,9 +1,26 @@
 require "clarifai_ruby/version"
+require 'clarifai_ruby/configuration'
 require 'httparty'
 
 module ClarifaiRuby
-  # Your code goes here...
-  def self.hello_world
-    "yo, boo"
+  def self.configuration
+    @configuration ||= Configuration.new
+  end
+
+  def self.configuration=(configuration)
+    @configuration = configuration
+  end
+
+  def self.configure
+    yield configuration
   end
 end
+
+# Usage
+#
+# ClarifaiRuby.configure do |config|
+#   config.base_url = "https://api.clarifai.com/v1"
+#   config.version_path = "/v1"
+#   config.client_id = "XDEWESBGHRDEWFGNBV"
+#   config.client_secret = "-123wedkfjnkj3"
+# end
---
+++ lib/clarifai_ruby/configuration.rb
@@ -0,0 +1,14 @@
+module ClarifaiRuby
+  class Configuration
+    attr_accessor :base_url, :client_id, :client_secret, :version_path
+
+    def initialize
+      @base_url = "https://api.clarifai.com"
+      @version_path = "/v1"
+    end
+
+    def api_url
+      base_url + version_path
+    end
+  end
+end
